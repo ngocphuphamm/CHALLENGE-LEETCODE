0767fae2760a1de25120e417cf3822164d0dc496
ngocphu <ngocphupham682001@gmail.com>
2022-01-12 19:14:06 +0700

    finish

--- Array/SquaresOfASortedArray/src/App.java
+++ Array/SquaresOfASortedArray/src/App.java
@@ -1,22 +1,20 @@
 // squares of a Sorted Array
 // https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
 public class App {
- 
+    public static void sort(int [] nums )
+    {
+
+    }
     public static int[] sortedSquares(int[] nums) {
         
-        
+      
         for(int i = 0 ; i< nums.length;i++)
         {
           
             nums[i]=  nums[i] * nums[i];
             
         }
-        for(int i = 0 ; i< nums.length;i++)
-        {
-          
-           System.out.println(nums[i]);
-            
-        }
+       sort(nums);
         
         
        return nums;
@@ -25,6 +23,10 @@ public class App {
     public static void main(String[] args) throws Exception {
        int [] arr =  new int [] {1,2,3,4,5};
       int n [] =  sortedSquares(arr);
-    
+        sort(n);
+        for(int i = 0 ; i < n.length ; i ++ )
+        {
+            System.out.println(n[i]);
+        }
     }
 }
