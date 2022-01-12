a9321002e5a0a0d2a1b2b7ea180ccf329d0ef9e1
ngocphu <ngocphupham682001@gmail.com>
2022-01-13 02:01:00 +0700

    4

---
+++ Array/MergeSortedArray/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+         int  i =  nums1.length;  
+         int j = nums2.length; 
+
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a [] = {1, 2,3 ,4 ,5 };
+        System.out.println(a.length);
+    }
+}
