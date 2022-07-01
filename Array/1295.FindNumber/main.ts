a260835569f3f77bbba2523f34be9693cd9a8374
ngocphuphamm <ngocphupham682001@gmail.com>
2022-07-01 20:02:56 +0700

    findNUmber

---
+++ Array/1295.FindNumber/main.ts
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+
+var arrayNumber   = [123,44,22,55];
+
+const excute = new MethodExcute(arrayNumber);
+
+const result = excute.excuteEvenNumber();
+console.log(result);
+5
